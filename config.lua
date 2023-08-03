dcd147ab560d3891b80b29953992cda7493aee1a
Galaxyy <103646943+Galaxyy2@users.noreply.github.com>
2023-08-03 19:13:31 +0100

    Update config.lua
    Ped Loco Fixed

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config = {}
 
 Config.UsePed = true
-Config.Ped = {493.0744, -1529.3860, 29.2714,"Sr Manel",47.6192,0x94562DD7,"a_m_m_farmer_01"}
+Config.Ped = {493.0744, -1529.3860, 28.2714,"Sr Manel",47.6192,0x94562DD7,"a_m_m_farmer_01"}
 Config.PedTargetLoc = vector3(493.0744, -1529.3860, 29.2714)
